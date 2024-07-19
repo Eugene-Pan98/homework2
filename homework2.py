09801cace32265fcc83c044ec33858befcf1d0b1
Eugene-Pan98 <vb02@list.ru>
2024-07-19 23:50:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+home_work = 12
+hours = 1.5
+course = 'Python'
+time = 1 / 2 ** 3
+print('Курс:',course,', всего задач:',home_work,', затрачено часов:',hours,', среднее время выполения',time,'часа')
